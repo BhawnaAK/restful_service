2208546788c8bc7ad4ac5262174d1c2c92a915d4
BhawnaAK <bhawna.09@gmail.com>
2023-10-05 23:23:00 +0530

    Checking without folder

---
+++ src/main/java/com/example/demo/user/User.java
@@ -0,0 +1,70 @@
+package com.example.demo.user;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+@Entity(name = "user_details")
+public class User {
+	
+	
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@Size(min=2, message = "Name should have atleast 2 characters")
+	//@JsonProperty("user_name")
+	private String name;
+	
+	@Past(message = "Birth Date should be in the past")
+	//@JsonProperty("birth_date")
+	private LocalDate birthDate;
+	
+	
+ 	
+	public User(Integer id, String name, LocalDate birthDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+	}
+	
+} 
---
+++ src/main/java/com/example/demo/user/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.demo.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+	
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+}
